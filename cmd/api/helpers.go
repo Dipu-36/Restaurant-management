e61b10f4a38ac8b72bdf6db421dd675ce72dbb08
Diptendu Pal <Diptendupal232@gmail.com>
2026-06-28 22:51:25 +0530

    added error handling

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
 	"net/http"
+	"strings"
 )
 
 type envelope map[string]interface{}
@@ -20,5 +24,73 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data envelo
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	w.Write(js)
+	return nil
+}
+
+// readJSON method is used fo reading the JSON that the client is trying to send us via POST
+// will be used in scnearios like where the client is trying to feedback or their Personal Info
+// like their Address, Phone number, Name etc or customizations or cutlery they want
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst interface{}) error {
+
+	// to prevent the APIs from DOS attacks a max read limit of 1MB has been set
+	maxBytes := 1_048_576
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	//initilaize the decoder
+	dec := json.NewDecoder(r.Body)
+	// calling DisallowUnknowFields methods helps us to prevent if the JSON from the client
+	// includes any fields which cannot be mapped to the target destination fo eg trying to
+	// write JSON fields which it is not supposed to write, if the client tries something
+	// like this the decoder will return an error instead of ignoring the field
+	dec.DisallowUnknownFields()
+
+	// decode the request body into the destination
+	err := dec.Decode(dst)
+
+	if err != nil {
+		var syntaxError *json.SyntaxError                     // struct therefore errors.As()
+		var unmarshalTypeError *json.UnmarshalTypeError       // struct therefore again errors.As()
+		var invalidUnmarshalError *json.InvalidUnmarshalError // not a struct therefore errors.Is()
+
+		switch {
+		// CLient sent malformed JSON like missing brackets in out payloads
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf("body contains badly formed-JSON (at character %d)", syntaxError.Offset)
+
+		// Also malformed JSON but caught differently by the stdlib (Known Golang issue)
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("body contains badly formed JSON")
+
+		// Client sent correct feilds but wrong type like table_number: four instead of 4
+		case errors.As(err, &unmarshalTypeError):
+			if unmarshalTypeError.Field != "" {
+				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Offset)
+			}
+			return fmt.Errorf("body contains incorrect JSON type (at charcter %d)", unmarshalTypeError.Offset)
+
+		// Client sent an empty JSON body
+		case errors.Is(err, io.EOF):
+			return errors.New("body must not be empty")
+
+		case strings.HasPrefix(err.Error(), "json: unknown field"):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unkown field")
+			return fmt.Errorf("body contains unkown key %s", fieldName)
+
+		case err.Error() == "http: request body too large":
+			return fmt.Errorf("body must not be larger than %d bytes", maxBytes)
+
+		// We mistakenly pass a non-nil pointer to a Decode() it will return invalidUnmarshlaTypeError
+		// since this is a programmer mistake we panic instead of sending this error to the handlers
+		case errors.As(err, &invalidUnmarshalError):
+			panic(err)
+		}
+	}
+
+	// Decoding 2nd time to catch the clients if they are sending multiple JSON vvalues in one body
+	err = dec.Decode(&struct{}{})
+	if err != nil {
+		return errors.New("body must only contain a single JSON value")
+	}
+
 	return nil
 }
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -23,11 +23,19 @@ type application struct {
 
 func main() {
 	var cfg config
-	// flag
+
+	// flag.InVar registers a command-line flag that binds directly to a variable
+	// There are 4 parameters :-
+	// 1st param is the pointer to the variable where the parsed value is being stored
+	// 2nd param is the flag name used on the CLi like -port=8080
+	// 3rd param is thedefault value and the last param is the helper text shown when -help is used
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
-	flag.StringVar(&cfg.env, "env", "development", "Envirionment (development | stagin | deployment)")
+	flag.StringVar(&cfg.env, "env", "development", "Envirionment (development | staging | deployment)")
 	flag.Parse()
 
+	// the 1st param is the destination where the logs will be written
+	// 2nd param is the prefix string will be used further in case of debugging and others
+	// 3rd param auto prepends time and date beofre the msg
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
 	app := &application{
@@ -43,7 +51,7 @@ func main() {
 		WriteTimeout: 10 * time.Second,
 	}
 
-	logger.Printf("Starting %s server on %s:", cfg.env, cfg.port)
+	logger.Printf("Starting %s server on %d:", cfg.env, cfg.port)
 
 	err := srv.ListenAndServe()
 	logger.Fatal(err)

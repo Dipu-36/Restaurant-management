99624a046772cd2c6f40cf32f832a7effe7334fd
Diptendu Pal <Diptendupal232@gmail.com>
2026-06-30 03:56:16 +0530

    updated routes

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,10 +9,46 @@ import (
 func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
 
-	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthCheckHandler)
-	router.HandlerFunc(http.MethodPost, "/v1/createdish", app.createDish) // for the admin ONLY
-	//router.HandlerFunc(http.MethodGet, "/", app.MenuHandler)
-	//router.HandlerFunc(http.MethodGet, "/v1/menu", app.MenuHandler)
+	// Health Check
+	router.HandlerFunc(
+		http.MethodGet,
+		"/v1/healthcheck",
+		app.healthCheckHandler,
+	)
+
+	// Authentication
+	router.HandlerFunc(
+		http.MethodPost,
+		"/v1/users",
+		app.registerUserHandler,
+	)
+
+	router.HandlerFunc(
+		http.MethodPut,
+		"/v1/users/activated",
+		app.activateUserHandler,
+	)
+
+	router.HandlerFunc(
+		http.MethodPost,
+		"/v1/tokens/authentication",
+		app.createAuthenticationTokenHandler,
+	)
+
+	// Restaurant
+	router.Handler(
+		http.MethodPost,
+		"/v1/dishes",
+		app.authenticate(
+			http.HandlerFunc(app.createDish),
+		),
+	)
+
+	// Future Routes
+	// router.Handler(http.MethodGet, "/v1/restaurants", app.authenticate(http.HandlerFunc(app.listRestaurants)))
+	// router.Handler(http.MethodPost, "/v1/orders", app.authenticate(http.HandlerFunc(app.createOrder)))
+	// router.Handler(http.MethodGet, "/v1/orders/:id", app.authenticate(http.HandlerFunc(app.getOrder)))
+	// router.Handler(http.MethodPost, "/v1/payments", app.authenticate(http.HandlerFunc(app.createPayment)))
 
 	return router
 }

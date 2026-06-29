35734f1dc1d1f0387a9ca0dfb1004619960c9f85
Diptendu Pal <Diptendupal232@gmail.com>
2026-06-29 18:28:42 +0530

    improved create route

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,7 +10,7 @@ func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthCheckHandler)
-	router.HandlerFunc(http.MethodPost, "/v1/app.create", app.createDish)
+	router.HandlerFunc(http.MethodPost, "/v1/createdish", app.createDish) // for the admin ONLY
 	//router.HandlerFunc(http.MethodGet, "/", app.MenuHandler)
 	//router.HandlerFunc(http.MethodGet, "/v1/menu", app.MenuHandler)
 
